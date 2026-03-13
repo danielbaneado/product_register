b9b7222ba52450efc295e54a7ca7815f0b1c15ec
danielbaneado <danielalzate076@gmail.com>
2026-03-13 14:53:57 -0500

    Tercer sprint
    Añadir y ver productos como función, bucle while para añadir cuantos productos desee el usuario

--- inventario.py
+++ inventario.py
@@ -1,8 +1,22 @@
+op= True
 products= []
-try:
-    name= input("Type product name: ")
-    price= float(input("Type product price: "))
-    stock= int(input("Type product quantity: "))
-except:
-    print("Invalid value! Try again")
-total_cost= price * stock
+def add_and_see(fproducts):
+    try:
+        name= input("Type product name: ")
+        price= float(input("Type product price: "))
+        stock= int(input("Type product quantity: "))
+        fproducts.append({"Name": name, "Price": price, "Stock": stock})
+        final_price= price * stock
+        for product in fproducts:
+            for names, details in product.items():
+                print(names, "-", details)
+        print("Final price -", final_price)
+    except:
+        print("Invalid value! Try again")
+        
+while op!= "n":
+    op= input("Add product (y/n): ").lower()
+    if op== "y":
+        add_and_see(products)
+    elif op== "n":
+        print("Bye")
