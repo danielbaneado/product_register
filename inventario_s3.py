c48867a47acee8e17d92337da5a54d6d0d7efcfe
danielbaneado <danielalzate076@gmail.com>
2026-03-29 16:25:59 -0500

    Primer commit
    Se añade update_product y delete_product, así como comentarios docstring

--- inventario_s3.py
+++ inventario_s3.py
@@ -2,11 +2,16 @@ op = True
 inventory = []
 
 def add_product(inventory, name, price, stock):
+    """
+    Adds product name, price and stock in a new dictionary at inventory list.
+    """
     inventory.append({"Product": name, "Price": price, "Stock": stock})
     print(name, "successfully added!")
-    return inventory
     
-def see_inventory(inventory):  
+def see_inventory(inventory):
+    """
+    If there's no products, program returns to main menu, else, function iterates in product list, showing all qualities.
+    """
     if not inventory:  
         print("There's no products added yet!")
         return
@@ -17,6 +22,10 @@ def see_inventory(inventory):
             print(qualities, "->", details) 
 
 def calculate_totals(inventory):
+    """
+    Same return as see_inventory.
+    Calculates an overall by all products price and stock, also shows the number of added products.
+    """
     if not inventory:
         print("There's no products added yet!")
         return
@@ -30,8 +39,13 @@ def calculate_totals(inventory):
     for total in inventory_total: 
         overall+= total
     print("Inventory total ->", overall, "\n", "-" * 30, "\nProducts added ->", product_quantity)
+    cart= (overall, product_quantity)
+    return cart
 
 def search_product(inventory, pname):
+    """
+    Searchs product by name, if the name of iterated product doesnt match with seeked product, program skip this one.
+    """
     if not inventory:  
         print("There's no products added yet!")
         return
@@ -44,6 +58,9 @@ def search_product(inventory, pname):
             print(qualities, "->", details)
 
 def update_product(inventory, uname, new_price, new_stock):
+    """
+    Modifies price and stock for product to update, receiving this parameters and rewriting dictionary.
+    """
     if not inventory:  
         print("There's no products added yet!")
         return
@@ -52,9 +69,11 @@ def update_product(inventory, uname, new_price, new_stock):
             product["Price"]= new_price
             product["Stock"]= new_stock
             print(uname, "successfully updated.")
-    return inventory
     
 def delete_product(inventory, dname):
+    """
+    Iterates in inventory list and delete product searching by name.
+    """
     if not inventory:  
         print("There's no products added yet!")
         return
